08080240305773e1c8c2d107570e65e2db66b36c
Rouge Lin <rouge_3877@outlook.com>
2024-12-29 10:07:12 +0800

    ✅ Add testcase for simple case (without hazard) and RAW case

---
+++ test/testcase/simple.asm
@@ -0,0 +1,31 @@
+lui x1, 74565
+add x0, x0, x0
+add x0, x0, x0
+add x0, x0, x0
+add x0, x0, x0
+sw x1, 0(x0)
+add x0, x0, x0
+add x0, x0, x0
+add x0, x0, x0
+add x0, x0, x0
+lw x2, 0(x0)
+add x0, x0, x0
+add x0, x0, x0
+add x0, x0, x0
+add x0, x0, x0
+addi x3, x0, 511
+addi x4, x0, 1023
+add x0, x0, x0
+add x0, x0, x0
+add x0, x0, x0
+add x0, x0, x0
+add x5, x3, x4
+or x6, x3, x4
+and x7, x3, x4
+slt x8, x3, x4
+xor x9, x3, x4
+sub x10, x3, x4
+add x0, x0, x0
+add x0, x0, x0
+add x0, x0, x0
+add x0, x0, x0
